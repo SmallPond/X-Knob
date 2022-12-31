8ef5364ee3b9b2c878d07c0d9f9038c0aa1b3f40
dingmos <db_smpond@163.com>
2022-12-31 10:17:53 +0800

    [lvgl] UI animations work correctly only with LV_TICK_CUSTOM

--- lib/lvgl/lv_conf.h
+++ lib/lvgl/lv_conf.h
@@ -85,7 +85,7 @@
 
 /*Use a custom tick source that tells the elapsed time in milliseconds.
  *It removes the need to manually update the tick with `lv_tick_inc()`)*/
-#define LV_TICK_CUSTOM 0
+#define LV_TICK_CUSTOM 1
 #if LV_TICK_CUSTOM
 #define LV_TICK_CUSTOM_INCLUDE "Arduino.h"         /*Header for the system time function*/
 #define LV_TICK_CUSTOM_SYS_TIME_EXPR (millis())    /*Expression evaluating to current system time in ms*/
