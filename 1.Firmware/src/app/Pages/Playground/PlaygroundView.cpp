56c751a0bd72f53e2c80a639d9fc88ab894b1067
dingmos <db_smpond@163.com>
2023-01-28 17:54:07 +0800

    [ui] fix bound view left bound ARC_START_ROTATION

--- 1.Firmware/src/app/Pages/Playground/PlaygroundView.cpp
+++ 1.Firmware/src/app/Pages/Playground/PlaygroundView.cpp
@@ -8,7 +8,7 @@ static int32_t MIN_VALUE = 0;
 static int32_t SCALE_LEFT_BOUND_TICKS = 200;
 static int32_t SCALE_ANGLE_RANGE = 140;
 static int32_t SCALE_RIGHT_BOUND_TICKS = SCALE_LEFT_BOUND_TICKS + SCALE_ANGLE_RANGE;
-static int32_t ARC_START_ROTATION = 180;
+static int32_t ARC_START_ROTATION = 120;
 /*
  * 默认视图显示： 圆点，原点所在位置 label_value
  * 此函数根据需要增加或 hidden 对象
