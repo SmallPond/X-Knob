fc591806412a81dc00f394a2eb4f94ab179ea25e
dingmos <db_smpond@163.com>
2023-03-01 23:06:53 +0800

    [flash] remove usb cdc init, support usb dowload(flash), fixes #5
    https://github.com/SmallPond/X-Knob/issues/5

--- 1.Firmware/src/main.cpp
+++ 1.Firmware/src/main.cpp
@@ -26,9 +26,7 @@ void setup() {
     HAL::Init();
     strip_init();
     // HWSerial.begin(115200);
-    usb_cdc_init();
     display_init();
-    // ui_init();
     App_Init();
     // INIT_DONE();
 }
